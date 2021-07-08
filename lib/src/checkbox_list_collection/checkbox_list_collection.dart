1173950d0f033c99d35b61f7451598672751b4f5
naanrot <eacheajmal@gmail.com>
2021-07-08 16:29:56 +0530

    Now CheckBoxListCollection is stateless widget.

--- lib/src/checkbox_list_collection/checkbox_list_collection.dart
+++ lib/src/checkbox_list_collection/checkbox_list_collection.dart
@@ -1,32 +1,26 @@
 import 'package:checkbox_collection/src/checkbox_list_collection/checkbox_list_view_default.dart';
 import 'package:flutter/material.dart';
 
-class CheckBoxListCollection extends StatefulWidget {
+class CheckBoxListCollection extends StatelessWidget {
   final List<String> data;
   final void Function(List<int>) onItemsClicked;
+  final List<int> checkedItems = [];
 
-  const CheckBoxListCollection(
+  CheckBoxListCollection(
       {required this.data, required this.onItemsClicked, Key? key})
       : super(key: key);
 
   @override
-  _CheckBoxListCollectionState createState() => _CheckBoxListCollectionState();
-}
-
-class _CheckBoxListCollectionState extends State<CheckBoxListCollection> {
-  final List<int> checkedItems = [];
-
-  @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: widget.data.length,
+      itemCount: data.length,
       itemBuilder: (context, index) {
         return CheckBoxListViewDefault(
           index: index,
-          title: widget.data[index],
+          title: data[index],
           onValueChange: (i, isChecked) {
             isChecked ? checkedItems.add(i) : checkedItems.remove(i);
-            widget.onItemsClicked(checkedItems);
+            onItemsClicked(checkedItems);
           },
         );
       },
